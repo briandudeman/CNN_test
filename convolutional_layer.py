2e8cfc990ed88fdf0a8bb56d9b1a36ed3f5c3b06
briandudeman <brianjl1944@gmail.com>
2026-01-29 19:14:55 -0800

    v1 of rewritten convolutional_layer, with handwritten convolve function(prob much slower), not tested yet

--- convolutional_layer.py
+++ convolutional_layer.py
@@ -33,37 +33,44 @@ class Conv_Layer:
 
   def forward(self, input: np.ndarray):
 
+    self.input = input
+
     # padding the input
-    padded_input = np.zeros(self.m + self.padding, self.n + self.padding)
-    padded_input[(self.padding):(self.padding + self.m + 1), (self.padding):(self.padding + self.n + 1)] = input
+    padded_input = np.zeros(self.d, self.m + self.padding, self.n + self.padding)
+    self.padded_input = padded_input[:, (self.padding):(self.padding + self.m + 1), (self.padding):(self.padding + self.n + 1)] = input
 
 
     self.out = copy.deepcopy(self.bias) # makes adding everything with the bias easier
     for i in range(self.f_num): # for each filter or each output
-      self.out[i] += self.convolve(input, self.filters[i], self.stride)
+      for j in range(self.d):
+        self.out[i] += self.convolve(padded_input[j], self.filters[i, j], self.stride)
     return self.out
 
-  # input is a d * m * n array, filter is d * k_m * k_n
-  def convolve(self, input: np.ndarray, filter: np.ndarray, stride: int):
-    convolve_out = np.zeros(self.m, self.n) # no support for higher dimensions, shouldn't need to be any
-    for i in range(0, self.m - self.k_m, stride): # subtracting kernel dimension to keep in bounds
-      for j in range(0, self.n  - self.k_n, stride):
-        convolve_out[i, j] = np.sum(input[:, (i):(i + self.k_m), (j):(j + self.k_n)] * filter)
+  # (2D) input is an m * n array, filter is somthing like k_m * k_n
+  def convolve(self, input: np.ndarray, filter: np.ndarray, stride: int, backprop: bool = False):
+    convolve_out = np.zeros(input.shape) # no support for higher dimensions, shouldn't need to be any
+    
+    for i in range(0, input.shape[0] - filter.shape[0], stride): # subtracting kernel dimension to keep in bounds
+      for j in range(0, input.shape[1] - filter.shape[0], stride):
+        convolve_out[i, j] = np.sum(input[(i):(i + filter.shape[0]), (j):(j + filter.shape[0])] * filter)
+
     return convolve_out
 
-  def backward(self, dLdZ, l_rate=.01): # should return the partial derivative of the loss with respect to the input to the layer, kernels/filters, and the biases(not using automatic differentiation)
-    self.dLdZ = dLdZ
-    self.dLdz_d, self.dLdZ_m, self.dLdZ_n = np.shape(dLdZ)
+
+  def backward(self, dLdO, l_rate=.01): # should return the partial derivative of the loss with respect to the input to the layer, kernels/filters, and the biases(not using automatic differentiation)
     #print("shape", np.shape(dLdZ))
     self.dLdF = np.zeros(np.shape(self.filters)) #gradient of loss with respect to filters/kernels of layer
-    self.dLdB = copy.deepcopy(self.dLdZ) #gradient of loss with respect to the biases
-    self.dLdX = np.zeros((self.d, self.m, self.n)) # gradient with respect to the input of the layer
+    self.dLdB = copy.deepcopy(dLdO) #gradient of loss with respect to the biases
+    self.dLdX = np.zeros(np.shape(self.input)) # gradient with respect to the input of the layer
+    
+    padded_dLdO = np.zeros(dLdO.shape[0], dLdO.shape[1] + self.padding, dLdO.shape[2] + self.padding)
+    padded_dLdO = padded_dLdO[:, (self.padding):(self.padding + dLdO.shape[1] + 1), (self.padding):(self.padding + dLdO.shape[2] + 1)] = dLdO
+    
+    
     for i in range(self.f_num):
       for j in range(self.d):
-        print(np.shape(signal.convolve2d(self.dLdZ[i], self.filters[i, j], "full")))
-        print(np.shape(self.filters[i, j]))
-        self.dLdX[j] += signal.convolve2d(self.dLdZ[i], self.filters[i, j], "full")
-        self.dLdF[i, j] = signal.correlate2d(self.input[j], self.dLdZ[i], "valid") #correlate is the filter regular, convolve is with it flipped 180 degrees
+        self.dLdX[j] += self.convolve(padded_dLdO[i], np.rot90(self.filters[i, j], 2, (0, 1)), self.stride) # TODO: the dimensions on this arent right, how to convolve 1 x m x n and d x m x n?
+      self.dLdF[i] = self.convolve(self.padded_input, dLdO, self.stride)
 
     self.filters -= l_rate * self.dLdF
     self.bias -= l_rate * self.dLdB
